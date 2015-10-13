d33e723d804da8e9fc761ca30ad201df2c40ffcd
Charles Hart <charleshart.lds@gmail.com>
2015-10-13 17:10:36 -0600

    Initial Commit

---
+++ SortingHat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SortingHat
+//
+//  Created by Charles Hart on 10/13/15.
+//  Copyright © 2015 busybusy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
